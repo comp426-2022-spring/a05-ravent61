1eef7cf8b7c2686bb05c8e5e3d25aefeadd1e66d
Raven Taylor <raven61@live..unc.edu>
2022-04-30 18:11:01 -0400

    served the index file

---
+++ coin.mjs
@@ -0,0 +1,105 @@
+/** Coin flip functions 
+ * This module will emulate a coin flip given various conditions as parameters as defined below
+ */
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+export function coinFlip() {
+  var random = Math.floor(Math.random() * 2);
+  if (random < 1) {
+    return 'heads';
+  }
+  return 'tails';
+}
+
+/** Multiple coin flips
+ * 
+ * Write a function that accepts one parameter (number of flips) and returns an array of 
+ * resulting "heads" or "tails".
+ * 
+ * @param {number} flips 
+ * @returns {string[]} results
+ * 
+ * example: coinFlips(10)
+ * returns:
+ *  [
+      'heads', 'heads',
+      'heads', 'tails',
+      'heads', 'tails',
+      'tails', 'heads',
+      'tails', 'heads'
+    ]
+ */
+
+export function coinFlips(flips) {
+  var results = [];
+  for (var i = 0; i < flips; i++) {
+    results[i] = coinFlip();
+  }
+  return results;
+}
+
+/** Count multiple flips
+ * 
+ * Write a function that accepts an array consisting of "heads" or "tails" 
+ * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+ * an object containing the number of each.
+ * 
+ * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+ * { tails: 5, heads: 5 }
+ * 
+ * @param {string[]} array 
+ * @returns {{ heads: number, tails: number }}
+ */
+
+export function countFlips(array) {
+  var results = {heads: 0, tails: 0};
+  for (var i = 0; i < array.length; i++) {
+    var coin = array[i];
+    if (coin == "heads") {
+      results.heads += 1;
+    }else{
+      results.tails += 1;
+    }
+  }
+  return results;
+}
+
+/** Flip a coin!
+ * 
+ * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+ * 
+ * @param {string} call 
+ * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+ * 
+ * example: flipACoin('tails')
+ * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+ */
+
+export function flipACoin(call) {
+  var flip = coinFlip();
+  var win_or_lose;
+  if (flip == call) {
+    win_or_lose = 'win';
+  }else{
+    win_or_lose = 'lose';
+  }
+  return {call: call, flip: flip, result: win_or_lose};
+
+}
+
+
+/** Export 
+ * 
+ * Export all of your named functions
+*/
--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-// Place your server entry point code here
-// Serve static HTML files
-app.use(express.static('./public'));
+import { coinFlip, coinFlips, countFlips, flipACoin } from "./coin.mjs";
+import express from "express";
+import minimist from "minimist";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.static("./public"));
+const args = minimist(process.argv.slice(2));
 
-var express = require("express")
-var app = express()
+const HTTP_PORT = args["port"] || 5555;
 
-const db = require("./database.js")
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-
-// Server port
-var HTTP_PORT = argv['port'] || 5555
-
-// Start server
+//start an app server
 const server = app.listen(HTTP_PORT, () => {
-   console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+  console.log("App listening on port %PORT%".replace("%PORT%", HTTP_PORT));
+});
+
+//default respnse for any other request
+
+app.get("/app/", (req, res) => {
+  res.status(200).json("200 OK");
+});
+
+app.get("/app/flip", (req, res, next) => {
+  res.status(200).json({ flip: coinFlip() });
+});
+
+app.get("/app/flips/:number", (req, res, next) => {
+  const flips = coinFlips(req.params.number);
+  const summary = countFlips(flips);
+  res.status(200).json({ raw: flips, summary: summary });
+});
+
+app.get("/app/flip/call/heads", (req, res, next) => {
+  res.status(200).json(flipACoin("heads"));
+});
+
+app.get("/app/flip/call/tails", (req, res, next) => {
+  res.status(200).json(flipACoin("tails"));
+});
+
+app.use(function (req, res) {
+  res.status(404).send("404 NOT FOUND");
 });
